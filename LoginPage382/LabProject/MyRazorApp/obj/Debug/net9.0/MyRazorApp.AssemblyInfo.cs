16d96ec680e82a833568445a8e905e4c58eb4c11
Sezer Atas <sezeratas@gmail.com>
2025-04-29 13:37:27 +0300

    week7Last

--- LoginPage382/LabProject/MyRazorApp/obj/Debug/net9.0/MyRazorApp.AssemblyInfo.cs
+++ LoginPage382/LabProject/MyRazorApp/obj/Debug/net9.0/MyRazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a42822d077222eef01cd626a72df4b596ea5c5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aecdac81d2d1ca4fbb33ecc0a2c409480a5ab19e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
